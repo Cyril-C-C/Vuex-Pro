e31359ac73bf80679908760d3ab9f222cfd6143f
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-11-13 02:32:05 +0100

    Progressive Changes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+      <product-list-one></product-list-one>
+      <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+    name: 'app',
+    components: {
+        'product-list-one': ProductListOne,
+        'product-list-two': ProductListTwo
+    }
+}
+</script>
+
+<style>
+body{
+    font-family: Ubuntu;
+    color: #555;
+}
+</style>
